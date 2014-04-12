9e2caadc4ec3c91118b0bfdfea8ecbe3a2eb6f3a
03t02 <thanhtung.tran100@gmail.com>
2014-04-12 20:41:20 +0200

    Add elem at the end of list, but segfault when try to show this one, have to fixe it

--- srcs/list.c
+++ srcs/list.c
@@ -5,7 +5,7 @@
 ** Login   <tran_1@epitech.eu>
 ** 
 ** Started on  Thu Apr  3 22:50:40 2014 tran_1
-** Last update Sat Apr 12 15:07:38 2014 tran_1
+** Last update Sat Apr 12 20:34:33 2014 tran_1
 */
 
 #include "../includes/main.h"
@@ -13,23 +13,21 @@
 void		add_elem(t_list **list, char *name)
 {
   t_list	*stock;
-  //  t_list	*tmp;
+  t_list	*tmp;
 
   if ((stock = malloc(sizeof(*stock))) == NULL)
     exit(0);
-  stock->name = name;//strdup(name);
-  //printf("%s\n", stock->name);
-  stock->next = *list;
-  *list = stock;
-  /* if (*list == NULL) */
-  /*   *list = stock; */
-  /* else */
-  /*   { */
-  /*     tmp = *list; */
-  /*     while (tmp->next != NULL) */
-  /* 	tmp = tmp->next; */
-  /*     tmp->next = stock; */
-  /*   } */
+  stock->name = name;
+  stock->next = NULL;
+  if (*list == NULL)
+    *list = stock;
+  else
+    {
+      tmp = *list;
+      while (tmp->next != NULL)
+	tmp = tmp->next;
+      tmp->next = stock;
+    }
 }
 
 void		show_list(t_list *list)
@@ -37,7 +35,7 @@ void		show_list(t_list *list)
   t_list	*tmp;
 
   tmp = list;
-  while (tmp->next != NULL)
+  while (tmp != NULL)
     {
       printf("%s\n", tmp->name);
       tmp = tmp->next;

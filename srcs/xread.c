dfacb2710ae4c1a67c0dd203040e1de24a36763e
03t02 <thanhtung.tran100@gmail.com>
2014-04-12 14:28:21 +0200

    Write header in .cor

--- srcs/xread.c
+++ srcs/xread.c
@@ -5,7 +5,7 @@
 ** Login   <tran_1@epitech.eu>
 ** 
 ** Started on  Sat Mar 29 13:48:12 2014 tran_1
-** Last update Thu Apr 10 11:57:14 2014 tran_1
+** Last update Sat Apr 12 13:22:13 2014 tran_1
 */
 
 #include "../includes/main.h"
@@ -18,6 +18,7 @@ void		xread(t_open *opn)
 {
   char		*name;
 
+  malloc_file(&opn);
   while ((name = get_next_line(opn->fd)))
     {
       my_putstr(name);

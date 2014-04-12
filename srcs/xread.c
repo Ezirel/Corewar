d6ea70e0a52751c67221e478ea290d7100838544
03t02 <thanhtung.tran100@gmail.com>
2014-04-12 14:29:02 +0200

    Nothing

--- srcs/xread.c
+++ srcs/xread.c
@@ -5,7 +5,7 @@
 ** Login   <tran_1@epitech.eu>
 ** 
 ** Started on  Sat Mar 29 13:48:12 2014 tran_1
-** Last update Sat Apr 12 13:22:13 2014 tran_1
+** Last update Sat Apr 12 14:28:58 2014 tran_1
 */
 
 #include "../includes/main.h"
@@ -18,7 +18,6 @@ void		xread(t_open *opn)
 {
   char		*name;
 
-  malloc_file(&opn);
   while ((name = get_next_line(opn->fd)))
     {
       my_putstr(name);
